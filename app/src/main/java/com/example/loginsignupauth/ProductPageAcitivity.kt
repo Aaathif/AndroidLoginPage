58a456fa26cc42397255aa18b4eb0a9f6c77e9ab
Aathif <aaathif115@gmail.com>
2023-03-22 12:29:38 +0530

    second commit after productPage

---
+++ app/src/main/java/com/example/loginsignupauth/ProductPageAcitivity.kt
@@ -0,0 +1,11 @@
+package com.example.loginsignupauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductPageAcitivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_page_acitivity)
+    }
+}
